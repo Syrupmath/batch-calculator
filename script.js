f94a12d5dc277f219d19a92843903886821218f6
Jeffrey Morgenthaler <jeff@jeffreymorgenthaler.com>
2024-08-16 17:07:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// sw.js
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     const ingredientList = document.getElementById("ingredient-list");
     const addIngredientButton = document.getElementById("add-ingredient");
